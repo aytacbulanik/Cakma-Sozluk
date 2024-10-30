7abf19df2ac652ba88bc8904fabc495abddd3399
Aytac Bulanik <aytacbulanik@hotmail.com>
2024-10-30 23:56:19 +0300

    wrote simple data in firestore

--- Cakma Sozluk/Controller/FikirEkleViewController.swift
+++ Cakma Sozluk/Controller/FikirEkleViewController.swift
@@ -6,30 +6,42 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class FikirEkleViewController: UIViewController {
-
     
+    let db = Firestore.firestore()
     @IBOutlet weak var segmentedController: UISegmentedControl!
     @IBOutlet weak var kullaniciAdiLabel: UILabel!
     @IBOutlet weak var fikirTextView: UITextView!
     @IBOutlet weak var paylasButtonOut: UIButton!
-    
+    var categoryArray = ["Gündem","Siyaset","Magazin","Spor"]
+    var chosenCategory : String = "Gündem"
     override func viewDidLoad() {
         super.viewDidLoad()
         
         paylasButtonOut.layer.cornerRadius = 10
         fikirTextView.layer.cornerRadius = 10
     }
-
+    
     @IBAction func segmentedChanged(_ sender: UISegmentedControl) {
+        chosenCategory = categoryArray[segmentedController.selectedSegmentIndex]
         
     }
     @IBAction func paylasButtonPressed(_ sender: UIButton) {
+        guard let fikirText = fikirTextView.text else { return }
+        db.collection("Fikirler").addDocument(data: [
+            "categoryName" : chosenCategory,
+            "fikir" : fikirText,
+        ]) { error  in
+            if let error {
+                print(error.localizedDescription)
+            }
+        }
         
     }
-    
 }
+
 
 
 
---
+++ Cakma Sozluk/Model/Fikir.swift
@@ -0,0 +1,13 @@
+//
+//  Fikir.swift
+//  Cakma Sozluk
+//
+//  Created by Aytaç Bulanık on 30.10.2024.
+//
+
+import Foundation
+
+struct Fikir {
+    var fikirText : String
+    var categoryName : String
+}
