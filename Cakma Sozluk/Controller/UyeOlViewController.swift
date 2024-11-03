d3e99335df3ef18801256658e81dccbdae9a40a8
Aytac Bulanik <aytacbulanik@hotmail.com>
2024-11-03 13:45:58 +0300

    create user with firebaseAuth

--- Cakma Sozluk/Controller/UyeOlViewController.swift
+++ Cakma Sozluk/Controller/UyeOlViewController.swift
@@ -6,19 +6,17 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class UyeOlViewController: UIViewController {
 
     
     @IBOutlet weak var emailTextField: UITextField!
-    
     @IBOutlet weak var passwordTextField: UITextField!
-    
     @IBOutlet weak var passwordAgainTextField: UITextField!
-    
     @IBOutlet weak var uyeOlButtonOut: UIButton!
-    
     @IBOutlet weak var vazgeçButtonOut: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,6 +25,21 @@ class UyeOlViewController: UIViewController {
     }
     
     @IBAction func uyeOlButtonPressed(_ sender: UIButton) {
+        guard let email = emailTextField.text , let password = passwordTextField.text , let passwordAgain = passwordAgainTextField.text else { return }
+        if password != passwordAgain {
+            print("Şifreler uyuşmuyor")
+            return
+        }
+        Auth.auth().createUser(withEmail: email, password: passwordAgain) { AuthDataResult, error in
+            if let error {
+                print("Hata: \(error.localizedDescription)")
+                return
+            } else {
+                print("kullanıcı oluşturuldu")
+            }
+            
+        }
+        
     }
     
     @IBAction func vazgecButtonPressed(_ sender: UIButton) {
