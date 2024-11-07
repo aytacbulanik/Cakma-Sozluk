ba4bb1c3c68f70285a9e70f700b363ee476ee976
Aytac Bulanik <aytacbulanik@hotmail.com>
2024-11-07 22:03:39 +0300

    fikir edit button config

--- Cakma Sozluk/Controller/FikirTableViewCell.swift
+++ Cakma Sozluk/Controller/FikirTableViewCell.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseFirestore
+import FirebaseAuth
 
 class FikirTableViewCell: UITableViewCell {
     
@@ -37,6 +38,17 @@ class FikirTableViewCell: UITableViewCell {
         begeniSayisiLabel.text = String(fikir.begeniSayisi)
         eklenmeTarihiLabel.text = showTarih(zaman: fikir.eklenmeTarihi)
         yorumSayisiLabel.text = String(fikir.yorumSayisi)
+        editImageView.isHidden = true
+        if fikir.kullaniciId == Auth.auth().currentUser?.uid {
+            editImageView.isHidden = false
+            editImageView.isUserInteractionEnabled = true
+            let tap = UITapGestureRecognizer(target: self, action: #selector(editImgPressed))
+            editImageView.addGestureRecognizer(tap)
+        }
+    }
+    
+    @objc func editImgPressed() {
+        print(secilenFikir.fikirText)
     }
     
     func showTarih(zaman : Date) -> String {
